69c051e655ee8a31d2cca2fdb4a9bb435e6360dc
kylecarbonell <76983718+kylecarbonell@users.noreply.github.com>
2022-08-17 22:13:40 -0700

    Update Turret.ino

--- Turret.ino
+++ Turret.ino
@@ -10,6 +10,23 @@ Servo servo;
 SerialData data(1,1);
 int vals[1];
 
+void turn(int vals){
+  servo.write(vals);
+  delay(20);
+  servo.write(vals-180);
+  
+}
+
+void shoot() {
+  digitalWrite(9, HIGH);
+//  digitalWrite(11, HIGH);
+//  digitalWrite(6, HIGH);
+  delay(10000); 
+  digitalWrite(9, LOW);
+//  digitalWrite(11, LOW);
+//  digitalWrite(6, LOW);
+}
+
 void setup() {
   // put your setup code here, to run once:
   servo.attach(SERVO_PIN);
@@ -18,13 +35,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  xVal = analogRead(VRX_PIN);
-  xVal = map(xVal, 0, 1023, 0, 180);
-  
-  //delay(20);
 
+  //Code to move gun and servos on gears
+//  xVal = analogRead(VRX_PIN);
+//  xVal = map(xVal, 0, 1023, 0, 180);
+  
+  
+
+//Code for face detection and shooting
   data.Get(vals);
-  servo.write(vals[0]);
-
+  Serial.println(vals[0]);
+  if(vals[0] == 0){
+    servo.write(180);
+    delay(100);
+    servo.write(0);
+  }
+  else{
+    Serial.println("Working");
+  }
   
+
+  delay(20);
 }
